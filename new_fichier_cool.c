299012cd5cc04abe9368a2c40b6b0f6221daeee2
Quentin Dutournier-Ottenin <quentin.dutournier-ottenin@epitech.eu>
2026-05-07 11:41:04 +0200

    feat: add a started file

---
+++ new_fichier_cool.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void pas_fini(int arg)
+{
+    if (1 == 1 && 2 != 3) {
+        dprintf(1, "All good !\n");
+        return;
+    }
+    if (0 / 0) {
+        dprintf(1, "How did that not crash???????????\n");
+    }
+
+    // code actuellement en construction
+}
